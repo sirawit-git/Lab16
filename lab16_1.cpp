1aec049946550f6c7b529c00130fb22eeff9521c
Champ <champsrw49@gmail.com>
2025-02-04 20:49:01 +0700

    Lab16 complete

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -4,31 +4,69 @@
 #include<iomanip>
 using namespace std;
 
-void showData(double *,int,int);
+void showData(double ,int,int);
 
-void randData(double *,int,int);
+void randData(double,int,int);
 
-void findRowSum(const double *,double *,int,int);
+void findRowSum(const double ,double,int,int);
 
-void findColSum(const double *,double *,int,int);
+void findColSum(const double ,double,int,int);
 
 int main(){
-	srand(time(0));
-	const int N = 6, M = 8;
-	double data[N][M] = {};
-	double sum1[N] = {};
-	double sum2[M] = {};
-	double *dPtr = data[0];
-	randData(dPtr,N,M);
-	showData(dPtr,N,M);
-	
-	cout << "---------------------------------------------\n";
-	
-	findRowSum(dPtr,sum1,N,M);
-	showData(sum1,N,1);
-	
-	cout << "---------------------------------------------\n";
-	
-	findColSum(dPtr,sum2,N,M); 
-	showData(sum2,1,M);
+    srand(time(0));
+    const int N = 6, M = 8;
+    double data[N][M] = {};
+    double sum1[N] = {};
+    double sum2[M] = {};
+    double dPtr = data[0];
+    randData(dPtr,N,M);
+    showData(dPtr,N,M);
+
+    cout << "---------------------------------------------\n";
+
+    findRowSum(dPtr,sum1,N,M);
+    showData(sum1,N,1);
+
+    cout << "---------------------------------------------\n";
+
+    findColSum(dPtr,sum2,N,M); 
+    showData(sum2,1,M);
+}
+
+void showData(doubled, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < NM; i++){
+        cout << d[i];
+        if(i % M == M-1) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(doublea, int N, int M) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            double data = (rand() % 1000) / 1000.0;
+            a[i * M + j] = data;
+        }
+    }
+}
+
+void findRowSum(const double a, doubleb, int N, int M) {
+    for (int i = 0; i < N; i++) {
+        double sum = 0;
+        for (int j = 0; j < M; j++) {
+            sum += a[i * M + j];
+        }
+        b[i] = sum;
+    }
+}
+
+void findColSum(const double a, doubleb, int N, int M) {
+    for (int j = 0; j < M; j++) {
+        double sum = 0;
+        for (int i = 0; i < N; i++) {
+            sum += a[i * M + j];
+        }
+        b[j] = sum;
+    }
 }
